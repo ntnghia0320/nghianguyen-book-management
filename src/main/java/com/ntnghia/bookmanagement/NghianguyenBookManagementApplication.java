e1d1bbc8031affb2589a826b4e4b5f20d23e2068
ntnghia <ntnghia0320@gmail.com>
2021-07-28 14:14:21 +0700

    init project

---
+++ src/main/java/com/ntnghia/bookmanagement/NghianguyenBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ntnghia.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NghianguyenBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NghianguyenBookManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntnghia/bookmanagement/NghianguyenBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntnghia.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NghianguyenBookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
